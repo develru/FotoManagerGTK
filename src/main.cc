ffda487ee399ae6ce5cbae6f3446cbde556c014e
Richard Schwalk <richard.schwalk@gmail.com>
2017-01-05 22:02:22 +0100

    The sceleton of the program

---
+++ src/main.cc
@@ -0,0 +1,15 @@
+#include <gtkmm.h>
+
+int main(int argc, char *argv[])
+{
+    auto app = Gtk::Application::create(argc, argv, "org.rschwalk.fotomanager.base");
+
+    Gtk::Window window;
+
+    window.set_default_size(200, 200);
+
+    return app->run(window);
+
+
+    return 0;
+}
