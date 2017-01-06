9852497d31c8d02f24696a3fcb6f071e4a194ff0
Richard Schwalk <richard.schwalk@gmail.com>
2017-01-06 21:32:27 +0100

    Set up with glade.

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,32 @@
-#include <gtkmm.h>
+#include <iostream>
+#include <gtkmm/application.h>
+#include <gtkmm/builder.h>
+#include <glibmm/fileutils.h>
+#include <gtkmm/window.h>
+#include "gui_manager.h"
 
 int main(int argc, char *argv[])
 {
     auto app = Gtk::Application::create(argc, argv, "org.rschwalk.fotomanager.base");
 
-    Gtk::Window window;
+    auto refBuilder = Gtk::Builder::create();
+    try
+    {
+        refBuilder->add_from_file("../src/fotomanager_window.ui");
+    }
+    catch(const Glib::FileError& ex)
+    {
+        std::cerr << "FileError: " << ex.what() << std::endl;
+        return 1;
+    }
 
-    window.set_default_size(200, 200);
+    Gtk::Window* m_window = nullptr;
+    refBuilder->get_widget("window", m_window);
+    if (m_window) {
+        app->run(*m_window);
+    }
 
-    return app->run(window);
-
+    delete m_window;
 
     return 0;
 }
